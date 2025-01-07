2d09c0027fc64782c38f8cda50c5bd9b50d3c8c0
Li Lin <guru.lin@gmail.com>
2025-01-07 13:07:17 -0800

    make module runnable. tweaked logging

---
+++ edgar_sleuth/__main__.py
@@ -0,0 +1,25 @@
+import json
+import logging
+import logging.config
+import sys
+from pathlib import Path
+
+import click
+
+logger = logging.getLogger(__name__)
+
+logger_config_path = Path(__file__).parent.parent / "logger_config.json"
+if logger_config_path.exists():
+    with open(logger_config_path, "r") as f:
+        logging.config.dictConfig(json.load(f))
+
+
+@click.command()
+@click.argument("action")
+@click.option("--dryrun", is_flag=True)
+def main(action: str, dryrun: bool) -> None:
+    logger.info(f"Running action {action} with dryrun={dryrun}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
