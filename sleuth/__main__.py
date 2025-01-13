05b468b3af41d75249d6e7501553bcfc3c4204df
Li Lin <guru.lin@gmail.com>
2025-01-13 10:04:19 -0800

    update logic for checking output_tag

--- sleuth/__main__.py
+++ sleuth/__main__.py
@@ -180,7 +180,7 @@ def main(
     if action not in ["load-index", "init-search-phrases"] and not input_tag:
         raise click.UsageError("--input-tag is required")
 
-    if action not in ["export"] and not tags:
+    if action not in ["load-index"] and not tags:
         raise click.UsageError("output tags is required")
 
     if action == "load-index":
