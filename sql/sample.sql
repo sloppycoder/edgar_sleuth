074224b0a2096e132075e62c54b873e0e5f8e867
Li Lin <guru.lin@gmail.com>
2025-03-14 22:49:14 +0900

    update sample script to use fund_cik_map

--- sql/sample.sql
+++ sql/sample.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS sampled_cik;
 CREATE TABLE sampled_cik AS WITH distinct_cik AS (
     SELECT DISTINCT cik
     FROM master_idx
-    WHERE date_filed BETWEEN '2023-01-01' AND '2024-12-31'
+    WHERE date_filed BETWEEN '2022-01-01' AND '2024-12-31'
+--    AND cik in (SELECT cik FROM fund_cik_map)
 ) -- Get all distinct `cik` values for the specified date range
 ---Randomly sample 10% of the distinct `cik` values
 SELECT cik
