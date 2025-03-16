d6c30828f2453c899a4cf265a477f0df2de38eab
Li Lin <guru.lin@gmail.com>
2025-03-16 13:03:14 +0900

    update sample logic

--- sql/sample.sql
+++ sql/sample.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS sampled_cik;
 CREATE TABLE sampled_cik AS WITH distinct_cik AS (
     SELECT DISTINCT cik
     FROM master_idx
-    WHERE date_filed BETWEEN '2022-01-01' AND '2024-12-31'
+    WHERE date_filed BETWEEN '2002-01-01' AND '2004-12-31'
     AND cik in (SELECT cik FROM fund_cik_map)
 ) -- Get all distinct `cik` values for the specified date range
 ---Randomly sample 10% of the distinct `cik` values
@@ -10,7 +10,7 @@ SELECT cik
 FROM distinct_cik
 ORDER BY random()
 LIMIT (
-        SELECT CEIL(0.2 * COUNT(*))
+        SELECT CEIL(0.1 * COUNT(*))
         FROM distinct_cik
     );
 -- Step 3: Insert deduped records into the new table
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS master_idx_sample;
 CREATE TABLE master_idx_sample AS
 SELECT DISTINCT *
 FROM master_idx
-WHERE date_filed BETWEEN '2023-01-01' AND '2024-12-31'
+WHERE date_filed BETWEEN '2003-01-01' AND '2004-12-31'
     AND cik IN (
         SELECT cik
         FROM sampled_cik
